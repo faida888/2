55ecc3b7a7b36e31544722d3c98af05372165cc2
faida888 <faida888@mail.ru>
2018-11-29 13:13:08 +0300

    11

--- src/frame1.java
+++ src/frame1.java
@@ -84,14 +84,14 @@ public class frame1 extends JApplet {
 		frame.getContentPane().setLayout(null);
 		
 		
-		final JRadioButton rdbtnNewRadioButton = new JRadioButton("РљРІР°СЂС‚РёСЂР°");
+		final JRadioButton rdbtnNewRadioButton = new JRadioButton("Квартира");
 		buttonGroup.add(rdbtnNewRadioButton);
 		rdbtnNewRadioButton.setSelected(true);
 		rdbtnNewRadioButton.setBounds(31, 51, 149, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton);
 	
 
-		final JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Р§Р°СЃС‚РЅС‹Р№ РґРѕРј");
+		final JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Частный дом");
 		buttonGroup.add(rdbtnNewRadioButton_1);
 		rdbtnNewRadioButton_1.setBounds(31, 79, 149, 23);
 		frame.getContentPane().add(rdbtnNewRadioButton_1);
@@ -127,7 +127,7 @@ public class frame1 extends JApplet {
 		});
 			
 		
-		JButton btnNewButton = new JButton("Р Р°СЃСЃС‡РёС‚Р°С‚СЊ");
+		JButton btnNewButton = new JButton("Рассчитать");
 		btnNewButton.addMouseListener(new MouseAdapter() {
 			public void mouseClicked(MouseEvent arg0) {
 				
@@ -143,7 +143,7 @@ public class frame1 extends JApplet {
 				double Gas_Flat = Double.parseDouble(textField_2.getText());
 				Price_Flat = Calculations.getPrice_Flat(Flat_Size, Hot_Water, Cold_Water, Gas_Flat, Electricity);
 			   	textField_8.setText(Double.toString(Price_Flat));
-			   	JOptionPane.showMessageDialog(null, "Р Р°СЃС‡РµС‚ РґР»СЏ РєРІР°СЂС‚РёСЂС‹ РІС‹РїРѕР»РЅРµРЅ");
+			   	JOptionPane.showMessageDialog(null, "Расчет для квартиры выполнен");
 			   	
 			   	saved = textField_8.getText();
 				JOptionPane.showMessageDialog(null, saved);
@@ -153,7 +153,7 @@ public class frame1 extends JApplet {
 			  
 			  if (rdbtnNewRadioButton_1.isSelected() == true)  {
 			    	{    				
-			    	JOptionPane.showMessageDialog(null, "Р Р°СЃС‡РµС‚ РґР»СЏ С‡Р°СЃС‚РЅРѕРіС€Рѕ РґРѕРјР° РІС‹РїРѕР»РЅРµРЅ");
+			    	JOptionPane.showMessageDialog(null, "Расчет для частногшо дома выполнен");
 
 			    		double Gas_House = Double.parseDouble(textField_5.getText());
 			   			Price_House = Calculations.getPrice_House(Cold_Water, Gas_House, Electricity);
@@ -180,53 +180,53 @@ public class frame1 extends JApplet {
 		btnNewButton.setBounds(319, 261, 117, 25);
 		frame.getContentPane().add(btnNewButton);
 		
-		JLabel lblNewLabel = new JLabel("Р’С‹Р±РµСЂРёС‚Рµ С‚РёРї СЃРѕР±СЃС‚РІРµРЅРЅРѕСЃС‚Рё");
+		JLabel lblNewLabel = new JLabel("Выберите тип собственности");
 		lblNewLabel.setBounds(30, 28, 366, 15);
 		frame.getContentPane().add(lblNewLabel);
 			
 		
 		
-		JLabel lblNewLabel_1 = new JLabel("Р“РѕСЂСЏС‡Р°СЏ РІРѕРґР°");
+		JLabel lblNewLabel_1 = new JLabel("Горячая вода");
 		lblNewLabel_1.setBounds(31, 110, 117, 15);
 		frame.getContentPane().add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("РҐРѕР»РѕРґРЅР°СЏ РІРѕРґР°");
+		JLabel lblNewLabel_2 = new JLabel("Холодная вода");
 		lblNewLabel_2.setBounds(31, 137, 120, 15);
 		frame.getContentPane().add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("РєСѓР±.Рј");
+		JLabel lblNewLabel_3 = new JLabel("куб.м");
 		lblNewLabel_3.setBounds(237, 110, 70, 15);
 		frame.getContentPane().add(lblNewLabel_3);
 		
-		JLabel lblNewLabel_4 = new JLabel("РєСѓР±.Рј");
+		JLabel lblNewLabel_4 = new JLabel("куб.м");
 		lblNewLabel_4.setBounds(237, 137, 70, 15);
 		frame.getContentPane().add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_5 = new JLabel("Р“Р°Р·");
+		JLabel lblNewLabel_5 = new JLabel("Газ");
 		lblNewLabel_5.setBounds(31, 163, 70, 15);
 		frame.getContentPane().add(lblNewLabel_5);
 		
-		JLabel lblNewLabel_6 = new JLabel("Р­Р»РµРєС‚СЂРёС‡РµСЃС‚РІРѕ");
+		JLabel lblNewLabel_6 = new JLabel("Электричество");
 		lblNewLabel_6.setBounds(31, 190, 117, 15);
 		frame.getContentPane().add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_7 = new JLabel("РєРІС‚*С‡Р°СЃ");
+		JLabel lblNewLabel_7 = new JLabel("квт*час");
 		lblNewLabel_7.setBounds(237, 190, 70, 15);
 		frame.getContentPane().add(lblNewLabel_7);
 		
-		JLabel lblNewLabel_8 = new JLabel("РєСѓР±.Рј");
+		JLabel lblNewLabel_8 = new JLabel("куб.м");
 		lblNewLabel_8.setBounds(237, 163, 70, 15);
 		frame.getContentPane().add(lblNewLabel_8);
 		
-		JLabel lblNewLabel_9 = new JLabel("РџР»РѕС‰Р°РґСЊ");
+		JLabel lblNewLabel_9 = new JLabel("Площадь");
 		lblNewLabel_9.setBounds(214, 55, 70, 15);
 		frame.getContentPane().add(lblNewLabel_9);
 		
-		JLabel lblNewLabel_10 = new JLabel("РєРІ.Рј");
+		JLabel lblNewLabel_10 = new JLabel("кв.м");
 		lblNewLabel_10.setBounds(355, 55, 70, 15);
 		frame.getContentPane().add(lblNewLabel_10);
 		
-		JLabel lblNewLabel_11 = new JLabel("С‚С‹СЃ.РєСѓР±.Рј");
+		JLabel lblNewLabel_11 = new JLabel("тыс.куб.м");
 		lblNewLabel_11.setBounds(355, 163, 83, 15);
 		frame.getContentPane().add(lblNewLabel_11);
 		
@@ -265,11 +265,11 @@ public class frame1 extends JApplet {
 		frame.getContentPane().add(textField_8);
 		textField_8.setColumns(10);
 		
-		JLabel lblNewLabel_12 = new JLabel("РС‚РѕРіРѕ");
+		JLabel lblNewLabel_12 = new JLabel("Итого");
 		lblNewLabel_12.setBounds(31, 231, 70, 15);
 		frame.getContentPane().add(lblNewLabel_12);
 		
-		JButton btnNewButton_1 = new JButton("Р’С‹С…РѕРґ");
+		JButton btnNewButton_1 = new JButton("Выход");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 		        System.exit(0);
