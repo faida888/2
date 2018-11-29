6b9157f3daf4d461d014ea63ab97955cdb331a35
faida888 <faida888@mail.ru>
2018-11-29 13:20:41 +0300

    222

--- src/frame1.java
+++ src/frame1.java
@@ -79,7 +79,7 @@ public class frame1 extends JApplet {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 500, 400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
